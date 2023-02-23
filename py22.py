d51e9a2e718f51897cf5b4ab931cd71fbbea63e5
halasznoel <93653964+halasznoel@users.noreply.github.com>
2023-02-23 09:41:11 +0100

    Add files via upload

---
+++ py22.py
@@ -0,0 +1,6 @@
+
+szöveg = input("Írj egy szöveget! ")
+szám = int(input("Hányszor akarod kiiratni a szöveget? "))
+
+kiiratott = szöveg*szám
+print(f"{szám} alkalommal kiiratva a szöveg:\n {kiiratott}")
